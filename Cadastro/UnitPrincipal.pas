99d9d1b1e2e0ed0ce9fc13c5c3fc6d0adbbb57e8
Gedsonfa <106558706+Gedsonfa@users.noreply.github.com>
2024-02-14 16:25:09 -0300

    Aula 9

---
+++ Cadastro/UnitPrincipal.pas
@@ -0,0 +1,97 @@
+unit UnitPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Label1: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    TabSheet4: TTabSheet;
+    Edit1: TEdit;
+    Label2: TLabel;
+    Label3: TLabel;
+    Edit2: TEdit;
+    Label4: TLabel;
+    ComboBox1: TComboBox;
+    Label5: TLabel;
+    Edit3: TEdit;
+    Label6: TLabel;
+    Edit4: TEdit;
+    Label7: TLabel;
+    Edit5: TEdit;
+    Label8: TLabel;
+    Edit6: TEdit;
+    Label9: TLabel;
+    Edit7: TEdit;
+    Label10: TLabel;
+    Label11: TLabel;
+    Edit8: TEdit;
+    Edit9: TEdit;
+    Edit10: TEdit;
+    Edit11: TEdit;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Edit12: TEdit;
+    CheckBox1: TCheckBox;
+    Label15: TLabel;
+    Edit13: TEdit;
+    Label16: TLabel;
+    Edit14: TEdit;
+    Label17: TLabel;
+    Edit15: TEdit;
+    GroupBox1: TGroupBox;
+    Label18: TLabel;
+    Edit16: TEdit;
+    Label19: TLabel;
+    Edit17: TEdit;
+    ComboBox2: TComboBox;
+    Label20: TLabel;
+    Label21: TLabel;
+    Edit18: TEdit;
+    Label22: TLabel;
+    Label23: TLabel;
+    Edit20: TEdit;
+    Label24: TLabel;
+    Edit21: TEdit;
+    Label25: TLabel;
+    Edit22: TEdit;
+    Edit19: TEdit;
+    Label26: TLabel;
+    Edit23: TEdit;
+    Label27: TLabel;
+    Edit24: TEdit;
+    Label28: TLabel;
+    Edit25: TEdit;
+    Label29: TLabel;
+    Edit26: TEdit;
+    Label30: TLabel;
+    Edit27: TEdit;
+    Memo1: TMemo;
+    Label31: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
